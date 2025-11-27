17e3bbd01f4394261d6c481b14078c09144a93de
Daniel Kuettel <dkuettel@gmail.com>
2025-11-27 16:01:08 +0100

    proper derivation

--- flake.nix
+++ flake.nix
@@ -2,19 +2,31 @@
   description = "POC for an easy and fast `nix develop` workflow.";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
-      # nixpkgs,
+      nixpkgs,
       flake-utils,
     }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages.default = ./pkg;
-    });
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "nd";
+          src = ./pkg;
+          installPhase = ''
+            cp -r $src $out
+          '';
+        };
+      }
+    );
   # TODO lets see if we can offer more useful integration
   # // {
   #   nixosModules.default =
