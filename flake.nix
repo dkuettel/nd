69bc805966d116e1b46e94dd90fe879883ec42f9
Daniel Kuettel <dkuettel@gmail.com>
2025-11-27 14:01:37 +0100

    poc

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "POC for an easy and fast `nix develop` workflow.";
+
+  inputs = {
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      # nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default = ./pkg;
+    });
+  # TODO lets see if we can offer more useful integration
+  # // {
+  #   nixosModules.default =
+  #     { config, ... }:
+  #     {
+  #       options = { };
+  #       config = { };
+  #     };
+  #
+  # };
+}
