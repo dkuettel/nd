a656170cbe3c23a9287e38ad53ff647fdbd5a2c3
Daniel Kuettel <dkuettel@gmail.com>
2026-08-05 13:41:21 +0200

    note

--- src/bin/nd-shell-env.rs
+++ src/bin/nd-shell-env.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 /// behaves like `nd --silent shell --env --build-if-missing -- $@`
+/// and should be a legal replacement for a shell
 fn main() {
     let args: Vec<String> = env::args().collect();
     nd::set_volume(true, true);
