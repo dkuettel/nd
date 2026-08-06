ef8511548cd75c5607a48973ed0c7b7849f7bab8
Daniel Kuettel <dkuettel@gmail.com>
2026-08-06 17:59:06 +0200

    comment

--- src/bin/nd-tmux-default-command.rs
+++ src/bin/nd-tmux-default-command.rs
@@ -1,4 +1,4 @@
-/// behaves like `nd --silent shell --maybe-env --build-if-missing --warn -- $@`
+/// behaves like `nd --silent shell --maybe-env --build-if-missing --warn`
 /// and should be a good candidate for tmux:
 /// > set-option -g default-command nd-tmux-default-command
 fn main() {
